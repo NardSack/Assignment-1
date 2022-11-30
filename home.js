fcbeddcc910410d1bde10f67e7f6fa8ad5f72a72
NardSack <S10242618@connect.np.edu.sg>
2022-12-01 00:46:32 +0800

    big update home page sdone

--- home.js
+++ home.js
@@ -25,7 +25,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  // setTimeout(showSlides, 20000); // Change image every 20 seconds
+  setTimeout(showSlides, 20000); // Change image every 20 seconds
 }
 
 function plusSlides(position) {
@@ -40,29 +40,21 @@ function plusSlides(position) {
   slides[slideIndex-1].style.display = "block";
 }
 
-// let slideIndex = 1;
-// showSlides(slideIndex);
 
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
 
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
 
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-// }
+
+var menuBtn = document.getElementById("menubutton")
+var setting = document.getElementById("setting")
+var menu = document.getElementById("menu")
+
+setting.style.top ="-1000px";
+
+menuBtn.onclick = function(){
+    if(setting.style.top =="-1000px"){
+      setting.style.top= "0";
+    }
+    else{
+      setting.style.top ="-1000px";
+    }
+}
---
+++ news.js
@@ -0,0 +1,24 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("socialmediacontainer").style.top = "0";
+  } else {
+    document.getElementById("socialmediacontainer").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+var menuBtn = document.getElementById("menubutton")
+var setting = document.getElementById("setting")
+var menu = document.getElementById("menu")
+
+setting.style.top ="-1000px";
+
+menuBtn.onclick = function(){
+    if(setting.style.top =="-1000px"){
+      setting.style.top= "0";
+    }
+    else{
+      setting.style.top ="-1000px";
+    }
+}
