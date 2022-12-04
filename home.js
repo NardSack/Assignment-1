48adaaa93b947008008e76018b35b1d59739d7e5
NardSack <S10242618@connect.np.edu.sg>
2022-12-04 23:15:28 +0800

    finish

--- home.js
+++ home.js
@@ -60,7 +60,9 @@ menuBtn.onclick = function(){
 }
 var login = document.getElementById("account")
 var loginpage = document.getElementById("loginpage")
+var back =document.getElementById("back")
 loginpage.style.left = "-100%"
+
 login.onclick = function() {
   if(loginpage.style.left =="-100%"){
     loginpage.style.left= "0";
@@ -69,4 +71,74 @@ login.onclick = function() {
     loginpage.style.left ="-100%";
    }
 }
+back.onclick = function() {
+  if(loginpage.style.left =="-100%"){
+    loginpage.style.left= "0";
+  }
+  else{
+    loginpage.style.left ="-100%";
+   }
+}
+var validpage = document.getElementById("arebot")
+var checkpass = document.getElementById("checkpass")
+var checkvali = document.getElementById("checkvali")
+validpage.style.right = "-100%"
+var submit = document.getElementById("submit")
+
+let mess = document.createElement("div")
+mess.classList.add('error')
+mess.innerHTML="Error Wrong input try again"
+
+
+submit.onclick= function(){
+  
+  var email = document.getElementById("email").value;
+  var name = document.getElementById("username").value;
+  var phone = document.getElementById("phone").value;
+  var password =document.getElementById("password").value;
+  if(email != '' &&name !=''&&phone!=''&&password!=''){
+  console.log("hello!!," +email+name+phone+password);
+  let ndiv = document.createElement("div")
+  ndiv.classList.add("changes")
+  ndiv.innerHTML = "Username: "+ name + "<br>Email: "+email+"<br>Phone: "+phone;
+  let sub = document.createElement("div")
+  sub.appendChild(ndiv)
+  document.getElementById("change").innerHTML = sub.innerHTML;
+  if(loginpage.style.left =="-100%"){
+    loginpage.style.left= "0";
+  }
+  else{
+    loginpage.style.left ="-100%";
+   }
+if(validpage.style.right =="-100%"){
+  validpage.style.right= "0";
+}
+else{
+  validpage.style.right ="-100%";
+   }}
+else{
+  document.getElementById("err").innerHTML="make sure to enter all the inputs with the correct details"
+}
+}
+
+checkvali.onclick=function(){
+  
+  if (checkpass.value == '5625')
+  
+    {
+      console.log('yayy')
+      if(validpage.style.right =="-100%"){
+      validpage.style.right= "0";
+    }
+    else{
+      validpage.style.right ="-100%";
+       }
+    }
+  else{
+    document.getElementById("errormess").innerHTML = mess.innerHTML
+  }
+}
+
+
+
 
